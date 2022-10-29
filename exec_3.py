19a0c55e4480c4850d6fee38f90f223b6ba72bfb
Sidinei de Andrade Silva <sidineiandradesilva@gmail.com>
2022-10-29 16:06:51 -0300

    Correção de espaco entre os comentarios

--- exec_3.py
+++ exec_3.py
@@ -1,13 +1,13 @@
-#aça um programa que converta da notação de 24 horas para a notação de 12 horas. 
-#Por exemplo, o programa deve converter 14:25 em 2:25 P.M. 
-#a) A entrada é dada em dois inteiros. 
-#b) Deve haver pelo menos duas funções: uma para a conversão e uma para a saída. 
-#c) Registre a informação A.M./P.M. como um valor "A" para A.M. e "P" para P.M. 
-#Assim, a função para efetuar as conversões terá um parâmetro formal para 
-#registrar se é A.M. ou P.M. 
-#d) Inclua um loop que permita que o usuário repita esse cálculo para novos valores 
-#de entrada todas as vezes que desejar, digitando um valor negativo para a hora 
-#quando quiser encerrar
+# faça um programa que converta da notação de 24 horas para a notação de 12 horas. 
+# Por exemplo, o programa deve converter 14:25 em 2:25 P.M. 
+# a) A entrada é dada em dois inteiros. 
+# b) Deve haver pelo menos duas funções: uma para a conversão e uma para a saída. 
+# c) Registre a informação A.M./P.M. como um valor "A" para A.M. e "P" para P.M. 
+# Assim, a função para efetuar as conversões terá um parâmetro formal para 
+# registrar se é A.M. ou P.M. 
+# d) Inclua um loop que permita que o usuário repita esse cálculo para novos valores 
+# de entrada todas as vezes que desejar, digitando um valor negativo para a hora 
+# quando quiser encerrar
 from modulos.util import  *
 while True:
     h = int(input('Hora: '))
